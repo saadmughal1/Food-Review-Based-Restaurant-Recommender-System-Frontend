e2ab84188742c7a9e6648d7a84350bf68eeded59
saad mughal <saadmughalxms@gmail.com>
2025-05-22 00:38:45 +0500

    delete admin related components and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,26 +9,16 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RecommendationsComponent } from './recommendations/recommendations.component';
 
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { AdminRestaurantManagementComponent } from './admin-restaurant-management/admin-restaurant-management.component';
-import { AdminUserManagementComponent } from './admin-user-management/admin-user-management.component';
+
 
 export const routes: Routes = [
-    { path: "", component: HomeComponent },
-    { path: "login", component: LoginComponent },
-    { path: "register", component: RegisterComponent },
-    { path: "restaurants", component: RestaurantCardComponent },
-    { path: "restaurants/:id", component: RestaurantDetailComponent },
-    { path: "profile", component: UserProfileComponent, canActivate: [AuthGuard] },
-    { path: "recommendations", component: RecommendationsComponent, canActivate: [AuthGuard] },
-    {
-      path: "admin",
-      component: AdminDashboardComponent,
-      canActivate: [AuthGuard, AdminGuard],
-      children: [
-        { path: "restaurants", component: AdminRestaurantManagementComponent },
-        { path: "users", component: AdminUserManagementComponent },
-      ],
-    },
-    { path: "**", redirectTo: "" },
-  ]
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "restaurants", component: RestaurantCardComponent },
+  { path: "restaurants/:id", component: RestaurantDetailComponent },
+  { path: "profile", component: UserProfileComponent, canActivate: [AuthGuard] },
+  { path: "recommendations", component: RecommendationsComponent, canActivate: [AuthGuard] },
+
+  { path: "**", redirectTo: "" },
+]
