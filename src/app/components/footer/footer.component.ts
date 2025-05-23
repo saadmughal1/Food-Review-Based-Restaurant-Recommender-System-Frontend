bc862cff987914dfa8297ba9e5c59d4dcaf956c2
saad mughal <saadmughalxms@gmail.com>
2025-05-23 21:38:39 +0500

    feat: enhance footer component with dynamic quick links

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,27 @@
-import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../auth/auth.service';
+import { map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   currentYear = new Date().getFullYear()
+
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.currentUser$.pipe(
+      map(user => !!user)
+    );
+  }
+
+
 
 }
